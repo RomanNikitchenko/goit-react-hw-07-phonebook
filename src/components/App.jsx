bba9f81f26c4cb7719d66c7307c905fe895921bf
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-06-14 11:49:40 +0300

    добавляю файлы с прошлого проекта

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
+import ContactForm from './form/contactForm';
+import Filter from './filter/filter';
+import ContactList from './contactList/contactList';
+
+const App = () => {
+  //   const [contacts, setContacts] = useState(() => {
+  //     //при первой загрузки получаем данные из локального хранилища либо записываем готовый массив контактов(обьектов)
+  //     return (
+  //       JSON.parse(window.localStorage.getItem('contacts')) ?? [
+  //         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //         { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //       ]
+  //     );
+  //   });
+
+  //   // первый раз при загрузки и каждый раз при изменении компонента перезаписываем в локальное хранилище
+  //   useEffect(() => {
+  //     window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  //   }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
+
+export default App;
---
+++ src/components/redux/phonebook/phonebook-types.jsx
@@ -0,0 +1,5 @@
+export default {
+  ADD_CONTACT: 'phonebook/addContact',
+  FILTER_CHANGE: 'phonebook/filterChange',
+  DELETE_CONTACT: 'phonebook/deleteContact',
+};
