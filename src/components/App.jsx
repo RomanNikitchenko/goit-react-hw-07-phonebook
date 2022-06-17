11e23f832c5fb2433b670e5702ec45cf18e7272b
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-06-17 20:39:03 +0300

    ht 7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,33 @@
-import Contacts from './pages/contacts';
+// import Contacts from './pages/contacts';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Contacts />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import {
+  useFetchContactsQuery,
+  useCreateContactMutation,
+} from './redux/phonebook/contactSlice';
+
+import ContactList from './contactList/contactList';
+import ContactForm from './form/contactForm';
+import Filter from './filter/filter';
 
 const App = () => {
+  const { data } = useFetchContactsQuery();
+  const [createContact] = useCreateContactMutation();
+
   return (
     <div>
-      <Contacts />
+      <ContactForm createContact={createContact} contacts={data} />
+      <Filter />
+      {data && <ContactList contacts={data} />}
     </div>
   );
 };
